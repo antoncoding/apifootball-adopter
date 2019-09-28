a0325fc70f6de0e0013b4fc05f57635a299c8314
Anton Cheng <anton@coolbitx.com>
2019-09-29 02:05:40 +0800

    Add usage text at get /

---
+++ README.md
@@ -0,0 +1,9 @@
+# APIFOOTBALL External Adopter for Epuesta
+
+Deployed on https://apifootball-adopter.herokuapp.com/.
+
+## Usage for Node Operator
+
+Create a new **Bridge** and connect it to our heroku sample server (or other serverless frameworks);
+
+![New Bridge](https://i.imgur.com/ZOisvWk.png)
--- app.js
+++ app.js
@@ -7,6 +7,17 @@ app.use(express.json());
 
 const { apiFootballAdopter } = require('./src/matchAdopter')
 
+app.get('/',(req, res)=>{
+  res.send(
+    `Sample Usage:\nPost /getMatch with 
+    {
+      "id":"278c97ffadb54a5bbb93cfec5f7b5503",
+      "data":{
+        "match_id":"224488"
+      }
+    }`)
+})
+
 app.post('/getMatch',(req, res) => { 
   console.log(req.body)
   apiFootballAdopter(req, res)
