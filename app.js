d3f4a2cc98a6be9e519c686bbe2750954d1c1717
Anton Cheng <anton@coolbitx.com>
2019-09-29 01:53:53 +0800

    Add port to .env

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 8080;
+
 app.use(express.json());
 
 
@@ -10,6 +12,6 @@ app.post('/getMatch',(req, res) => {
   apiFootballAdopter(req, res)
 });
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000...');
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
 });
